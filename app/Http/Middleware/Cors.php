d436fe959a363f9ec58bd34e3a82c6aaa1987bf2
Rafael Dutra Jardim <dutrajardim@hotmail.com>
2017-06-26 01:22:28 -0300

    adicionado Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,7 +15,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $domains = ['http://localhost:8080'];
+        $domains = ['http://localhost:8080','https://ensalamentoapp.herokuapp.com'];
 
         if (isset($request->server()['HTTP_ORIGIN'])) {
             $origin = $request->server()['HTTP_ORIGIN'];
